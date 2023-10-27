76238db8edbf560ec43819b2a060e4b745bb2003
Obote <obotedenis256@gmail.com>
2023-10-27 11:54:11 +0300

    init

---
+++ src/components/Form.jsx
@@ -0,0 +1,13 @@
+const Form = () => {
+  return (
+    <>
+      <h1>React Form Handling</h1>
+      <form>
+        <label>
+          First Name: <inout type="text" />
+        </label>
+      </form>
+    </>
+  );
+};
+export default Form;
---
+++ src/components/inputTodo.jsx
@@ -0,0 +1,4 @@
+const InputTodo = () => {
+  return <div>Input field here</div>;
+};
+export default InputTodo;
